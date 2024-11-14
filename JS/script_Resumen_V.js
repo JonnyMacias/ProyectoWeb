1b9ea3104d75a8f81edc3a3f57180b3132f44fa8
JonnyMacias <jonnymacias100@gmail.com>
2024-11-14 17:34:31 -0600

    readme.md actualizado

--- JS/script_Resumen_V.js
+++ JS/script_Resumen_V.js
@@ -1,8 +1,8 @@
 // Selecciona el elemento canvas para la gráfica
-const ctx = document.getElementById('pieChart').getContext('2d');
+const ctxPastel = document.getElementById('pieChart').getContext('2d');
 
 // Crea la gráfica de pastel
-const pieChart = new Chart(ctx, {
+const pieChart = new Chart(ctxPastel, {
     type: 'pie', // Cambia el tipo de gráfica a 'pie' o 'doughnut' para una gráfica de dona
     data: {
         labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'], // Etiquetas de los datos
@@ -37,3 +37,32 @@ const pieChart = new Chart(ctx, {
         }
     }
 });
+
+/*---------------GRAFICA DE LINEAS------------------------*/
+// Selecciona el elemento canvas para la gráfica
+const ctx = document.getElementById('lineChart').getContext('2d');
+
+// Crea la gráfica de líneas
+const lineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'], // Etiquetas de los datos
+        datasets: [{
+            label: 'Ventas en USD',
+            data: [120, 190, 300, 500, 200, 300], // Datos de la gráfica
+            borderColor: 'rgba(75, 192, 192, 1)', // Color de la línea
+            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Color del área bajo la línea
+            borderWidth: 2,
+            fill: true // Rellena el área bajo la línea
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true // Inicia el eje Y desde cero
+            }
+        }
+    }
+});
+
