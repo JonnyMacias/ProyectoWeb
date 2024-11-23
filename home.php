38ddea8bab270ad2f87703ae3cf77c333ff9e5c9
Bryan5891 <Barayan201404@gmail.com>
2024-11-23 00:26:23 -0600

    se agrego un archivo llamado inevtario y tambien una archivo llamado estilios_inventario, se modifico tanto el home como su estilo y se agrgaron imagenes

--- home.php
+++ home.php
@@ -14,13 +14,54 @@
     <body>
         <header>
             <nav>
-                <div class="nav-links">
-                    <a href="#">Productos</a>
-                    <a href="#">Misión</a>
-                    <a href="#">Visión</a>
-                    <a href="#">Valores</a>
-                    <a href="#">Contacto</a>
-                </div>
+            <ul class="menu-horizontal">
+                    <li><a href="home.php">Inicio</a></li>
+                    <li><a href="inventario.php">Productos</a>
+                    <ul class="menu-vertical">
+                                <li  class="">
+                                    <a href="#">Contenedores</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Troqueladas</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Caja Regular Ranurada</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Separadores / Rejillas</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Tarima de Cartón</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Tarima de Madera</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Cartón Plastificado</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Cartón Antiestático</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Cartón Milchelman</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Diseño Interno</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Plástico Corrugado</a>
+                                </li>
+                                <li  class="">
+                                    <a href="#">Bolsas VCI</a>
+                                </li>
+                            </ul>
+
+                </li>
+                    <li><a href="#">Misión</a></li>
+                    <li><a href="#">Visión</a></li>
+                    <li><a href="#">Valores</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
                 <?php if (isset($_SESSION['usuario'])): ?>
                     <div class="user-info">
                         <i class='bx bxs-user'></i>
