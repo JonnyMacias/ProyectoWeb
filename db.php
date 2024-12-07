5d86ac38265001f9387f2a42284950c254fb6298
JonnyMacias <jonnymacias100@gmail.com>
2024-12-07 15:33:50 -0600

    readme.md actualizado

--- db.php
+++ db.php
@@ -1,7 +1,11 @@
 <?php
 
+$servername = "mano.cjkioe6eoc42.us-east-1.rds.amazonaws.com:3306";
+$username = "root";
+$password = "3Hermanos*";
+$database = "edkena";
 
- $conexion = mysqli_connect("localhost","root","","edkena");
+ $conexion = mysqli_connect("mano.cjkioe6eoc42.us-east-1.rds.amazonaws.com:3306","root","3Hermanos*","edkena");
  /*if ($conexion){
 
     echo 'conectado exitosamente';
@@ -14,7 +18,7 @@
 
  try{
 
-   $pdo = new PDO("mysql:host=localhost;dbname=edkena", "root", "",
+   $pdo = new PDO("mano.cjkioe6eoc42.us-east-1.rds.amazonaws.com:3306;dbname=edkena", "root", "3Hermanos*",
    array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
 
     //echo "<script>alert('Conectado...')</script>";
